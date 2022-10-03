21e38e2d037c014647099f2c1212c54ace1bd303
aleksander-omelchuk <aleksander.omelchuk@gmail.com>
2022-10-03 17:59:15 +0300

    comit-task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+
+const makeIngredients = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemRef = document.createElement("li");
+    itemRef.textContent = ingredient;
+    itemRef.classList.add("item");
+    return itemRef;
+  });
+};
+
+const elements = makeIngredients(ingredients);
+ingredientsRef.append(...elements);
+console.log(ingredientsRef);
+

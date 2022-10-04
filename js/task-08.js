c41a8027a572c059b525d4e16c0836e4eff4569e
aleksander-omelchuk <aleksander.omelchuk@gmail.com>
2022-10-04 14:40:09 +0300

    finish-hw-06

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form')
+form.addEventListener('submit', handleSubmit)
 
+function handleSubmit (evn) {
+    evn.preventDefault()
+    const {
+        elements: {email, password},
+    } = evn.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert ('Please fill in all the fields')
+
+    }
+
+    const userDetails = {
+        email: email.value,
+        password: password.value
+    }
+
+    console.log(userDetails)
+    evn.currentTarget.reset()
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const inputNum = document.querySelector('#controls>input')
+const buttonCreate = document.querySelector('button[data-create]')
+const buttonDestroy = document.querySelector('button[data-destroy]')
+const boxes = document.querySelector('#boxes')
+
+
+const createBoxes = amount => {
+  const elementsToAdd = []
+  for (let i=0; i < amount; i++) {
+    const div = document.createElement('div')
+    div.style.height = `${30+10*i}px`
+    div.style.width = `${30+10*i}px`
+    div.style.background = getRandomHexColor()
+    elementsToAdd.push(div)
+  }
+  return elementsToAdd
+}
+
+const destroyBoxes = () => {
+  boxes.innerHTML = ''
+}
+
+buttonCreate.addEventListener('click', () => {
+  let boxesToAdd = createBoxes(inputNum.value)
+  boxes.append(...boxesToAdd)
+});
+
+console.log(inputNum.value)
+buttonDestroy.addEventListener('click',() => {
+  destroyBoxes.call()
+});

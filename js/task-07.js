e3bdb7ed0e5c367096d0c4af0a3c5b7e1f9d542e
aleksander-omelchuk <aleksander.omelchuk@gmail.com>
2022-10-05 19:21:40 +0300

    comit

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,29 @@
+const elem = {
+    inputChange:document.querySelector ('input#font-size-control'),
+    abracadabra:document.querySelector('span#text'),
+}
 
-const inputChange =document.querySelector ('input#font-size-control');
-const abracadabra = document.querySelector('span#text');
+elem.input.addEventListener('change', onInputValueChange);
+function onInputValueChange(event) {
+    elem.span.style.fontSize = `${event.currentTarget.value}px`;
+}
 
-inputChange.addEventListener('change', event => {
-    abracadabra.style.fontSize = `${event.target.value}px`
-    console.log(abracadabra)
-})
+// inputChange.addEventListener('change', event => {
+//     abracadabra.span.style.fontSize = `${event.currentTarget.value}px`
+// inputTextRef.span.style.fontSize = inputControlRef.value + 'px';
+
+// })
+
+
+
+
+// const refs = {
+//     input: document.querySelector('input#font-size-control'),
+//     span: document.querySelector('span#text'),
+//   };
+  
+//   refs.input.addEventListener('input', onInputValueChange);
+  
+//   function onInputValueChange(event) {
+//     refs.span.style.fontSize = `${event.currentTarget.value}px`;
+//   }
